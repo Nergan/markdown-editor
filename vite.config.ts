43fefd38f670da01d54c241a9eece6f2426edd56
Nergan <52855237+Nergan@users.noreply.github.com>
2025-07-05 19:37:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default defineConfig({
   plugins: [
     basicSsl(),
-    nodePolyfills(),
-  ]
+    nodePolyfills()
+  ],
+  base: "/markdown-editor/"
 })
